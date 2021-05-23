648c6d35b4b5387928f7d4d22c620c43e70684e2
aditya77kushwaha <adityakushwaha2912@gmail.com>
2021-05-23 13:38:37 +0530

    Added Routers

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const helmet = require('helmet')
 const morgan = require('morgan')
+const userRoute = require('./routes/users')
 
 dotenv.config()
 
@@ -15,8 +16,6 @@ mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopolo
 app.use(express.json())
 app.use(helmet())
 app.use(morgan("common"))
-
-
 
 app.listen(8800,()=>{
     console.log("Backend server is running!")
