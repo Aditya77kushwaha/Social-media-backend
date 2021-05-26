b339366587b6c7e04732b09ed1e383a046ebb357
aditya77kushwaha <adityakushwaha2912@gmail.com>
2021-05-26 12:28:57 +0530

    update a post schema and route

--- routes/posts.js
+++ routes/posts.js
@@ -1,7 +1,35 @@
 const router = require("express").Router();
+const Post = require("../models/Post");
 
-router.get("/", (req, res) => {
-  res.send("post page");
+//create a post
+router.post("/", async (req, res) => {
+  const newPost = new Post(req.body);
+  try {
+    const savedPost = await newPost.save();
+    res.status(200).json(savedPost);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
+
+//update a post
+router.put("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await post.updateOne({ $set: req.body });
+      res.status(200).json("the post has been updated");
+    } else {
+      res.status(403).json("you can update only your post");
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+//delete a post
+//like a post
+//get a post
+//get timeline(all) posts
 
 module.exports = router;
