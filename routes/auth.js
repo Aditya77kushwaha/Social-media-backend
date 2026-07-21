9552d14e5b1bcdc0befd2dede0edc1e838482fdf
Aditya Kushwaha <adityakushwaha2912@gmail.com>
2026-07-21 20:03:13 +0530

    fixes auth

--- routes/auth.js
+++ routes/auth.js
@@ -18,24 +18,47 @@ router.post("/register", async (req, res) => {
     console.log(newUser);
     //save user and respond
     const user = await newUser.save();
-    res.status(200).json(user);
+    return res.status(200).json(user);
   } catch (err) {
-    res.status(500).json(err)
+    console.error(err);
+    return res.status(500).json(err);
   }
 });
 
 //LOGIN
+// router.post("/login", async (req, res) => {
+//   try {
+//     const user = await User.findOne({ email: req.body.email });
+//     !user && res.status(404).json("user not found");
+
+//     const validPassword = await bcrypt.compare(req.body.password, user.password)
+//     !validPassword && res.status(400).json("wrong password")
+
+//     res.status(200).json(user)
+//   } catch (err) {
+//     res.status(500).json(err)
+//   }
+// });
 router.post("/login", async (req, res) => {
   try {
     const user = await User.findOne({ email: req.body.email });
-    !user && res.status(404).json("user not found");
 
-    const validPassword = await bcrypt.compare(req.body.password, user.password)
-    !validPassword && res.status(400).json("wrong password")
+    if (!user) {
+      return res.status(404).json("User not found");
+    }
 
-    res.status(200).json(user)
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password,
+    );
+
+    if (!validPassword) {
+      return res.status(400).json("Wrong password");
+    }
+
+    return res.status(200).json(user);
   } catch (err) {
-    res.status(500).json(err)
+    return res.status(500).json(err);
   }
 });
 
